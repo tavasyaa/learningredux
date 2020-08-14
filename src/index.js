6932e1578ba87039d0ebd01532b527b9f856121e
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-08-14 18:05:55 -0500

    just started learnin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+// store - global state
+
+// action - describes what you want to do. - increment. it's a function that returns an object, that's the name.
+// these don't actually do anything for now.
+const increment = () => {
+	return {
+		type: 'INCREMENT'
+	}
+}
+
+const decrement = () => {
+	return {
+		type: 'DECREMENT'
+	}
+}
+
+// reducer - describes how an action transforms a state into the next
+const counter = (state = 0, action) => {
+	switch(action.type){
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT':
+			return state - 1;
+	}
+}
+
+let store = createStore(counter);
+
+// display on console
+store.subscribe(() => console.log(store.getState()));
+
+// dispatch - execute the action (dispatch the action to the reducer)
+store.dispatch(increment())
+
 
 ReactDOM.render(
   <React.StrictMode>
